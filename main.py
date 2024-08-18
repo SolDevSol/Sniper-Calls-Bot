4053c81f20a3b8f56582fd80d3c845e74db9bd57
SolanaDev <nft.sol@rambler.ru>
2024-08-18 23:46:54 +0300

    v1.1.6

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def check_socket():
         port = random.randint(5000, 10000)
 
         with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+            sock.settimeout(1)
             if sock.connect_ex((host, port)) == 0:
                 return port
                 # print("Port is open")
